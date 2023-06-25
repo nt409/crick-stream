660b8cbc51f0af9c0edd1374a668872ffb851821
nt409 <44670661+nt409@users.noreply.github.com>
2023-06-25 18:07:37 +1000

    working

--- run.py
+++ run.py
@@ -13,21 +13,19 @@ from fns import get_data, write_files
 
 # Start of each day's play:
 # ! 1. Press green play button (top right)
-# ! 2. If you need to cancel/reset, press "ctrl+c". Might have to click in the "TERMINAL" box below before clicking "ctrl+c".
+# ! 2. If you need to cancel/reset, click in the "TERMINAL" box below and type "ctrl+c"
 
-
-# * CONFIG VARIABLES: "url" and "sleeptime"
+# * CONFIG VARIABLES: URL and SLEEP_TIME
 
 # ! to update each game:
-# * UR:: link to play-cricket scorecard
+# * URL: link to play-cricket scorecard
 # Find on Cam Uni play-cricket: https://cambridgeuniversity.play-cricket.com/Matches
 
 URL = "https://cambridgeuniversity.play-cricket.com/website/results/5690698"
 
-
 # ! leave this alone!
-# * sleeptime: amount of time between checks of the website (in seconds)
-# fairly long sleeptime reduces number of times we spam the website, probably a good idea
+# * SLEEP_TIME: amount of time between checks of the website (in seconds)
+# long SLEEP_TIME reduces number of times we spam the website, probably a good idea
 
 SLEEP_TIME = 40
 
@@ -46,8 +44,6 @@ def main(url):
 if __name__ == "__main__":
 
     while True:
-        # main(url)
-
         try:
             main(URL)
         except Exception as e:
